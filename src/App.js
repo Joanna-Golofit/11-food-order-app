fe9a5c8e6ce04a7300df7b9ca17cc5340d119917
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-18 15:30:32 +0200

    136. Adding Individual Meal Items & Displaying Them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
   );
 }
 

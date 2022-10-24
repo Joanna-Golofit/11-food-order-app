776d313d67ceb6f8099dfd47679ac7eecf4a99c6
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-24 13:15:48 +0200

    145. Working with Refs & Forward Refs (useContext and useReducer)

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
 
 
 const MealItem = (props) => {
-  // console.log(props)
+  const cartCtx = useContext(CartContext)
+  // console.log(cartCtx)
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = amount => {
+    console.log(amount)
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    });
+    }
 
   return (
     <li className={classes.meal}>
@@ -15,7 +27,7 @@ const MealItem = (props) => {
         <div className={classes.price} >{price}</div>
       </div>
       <div>
-        <MealItemForm {...props} />
+        <MealItemForm {...props} onAddToCart={addToCartHandler} />
         {/* <MealItemForm key={props.key} /> */}
       </div>
     </li>

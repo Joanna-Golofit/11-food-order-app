27610af9caec4e7dbc579cad2b5788ff1df08f0d
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-25 12:40:18 +0200

    my version of bumping the cart button with useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((currNumber, item)=> currNumber + item.amount, 0)
+  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => currNumber + item.amount, 0)
+  
+  let btnClasses = `${classes.button} ${btnIsHighlighted && classes.bump}`;
+  useEffect(() => {
+    setBtnIsHighlighted(true)
+    setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 300);
+
+  
+    // return () => {
+
+    // }
+  }, [numberOfCartItems])
+  
 
   console.log("cartCtx", cartCtx)
   return (
-    <button className={classes.button} onClick={props.onClick} >
+    <button className={btnClasses} onClick={props.onClick} >
       <span className={classes.icon}><CartIcon/></span>
       <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
